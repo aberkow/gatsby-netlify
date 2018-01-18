2c9839cd245729023e74398eff28cffb343ce6a3
aberkow <berkowitz.clarinet@gmail.com>
2018-01-17 21:04:20 -0500

    nope

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -27,17 +27,6 @@ const TemplateWrapper = ({ children }) => (
       }}
     >
       {children()}
-      <script>
-        if (window.netlifyIdentity) {
-          window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-              });
-            }
-          })
-        }
-      </script>
     </div>
   </div>
 )
