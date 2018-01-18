bb8c6f4dd5d5f5cb574f7ef3621502cf3ec2fece
aberkow <berkowitz.clarinet@gmail.com>
2018-01-17 20:56:58 -0500

    fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -27,18 +27,18 @@ const TemplateWrapper = ({ children }) => (
       }}
     >
       {children()}
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
     </div>
-    <script>
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-              document.location.href = "/admin/";
-            });
-          }
-        });
-      }
-</script>
   </div>
 )
 
